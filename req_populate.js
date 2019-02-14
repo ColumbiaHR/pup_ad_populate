0a73f5d5884b5c18db1795b450b8b12b42a937f3
ColumbiaHR <47584096+ColumbiaHR@users.noreply.github.com>
2019-02-13 20:04:29 -0500

    Add fonts

--- req_populate.js
+++ req_populate.js
@@ -116,7 +116,7 @@ var pupPdElements = {
  * Create template w/ object values inserted
  * insert a clean table w/ id's. Note: this was a nice multi-line string but didn't function well as a bookmarklet
  */
-newBody  = '<ul style="color: #2c6bac;">';
+newBody  = '<ul style="color: #2c6bac; font-family: arial, helvetica, sans-serif; font-size: 9pt;">';
 newBody += '<li style="color: #ff0000;" id="pupPdJobType">Job Type: ' + pupPdElements.pupPdJobType  + '</li>';
 newBody += '<li id="pupPdBargainingUnit">Bargaining Unit: ' + pupPdElements.pupPdBargainingUnit + '</li>';
 newBody += '<li id="pupPdRegTemp">Regular/Temporary: ' + pupPdElements.pupPdRegTemp  + '</li>';
